7b37874c8f64c32e0fd154bbda37379e48a898f9
andriuscrka <andrius.crka@gmail.com>
2024-11-20 17:17:05 +0200

    remove starter code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: false,
+        html: false,
+      },
+    ],
+  },
 };
